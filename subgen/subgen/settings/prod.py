d216f3e0912981d598720bd1752494def76d6ced
unknown <lselkins@crimson.ua.edu>
2021-09-12 13:25:57 -0500

    Adjuseted secret key logic

--- subgen/subgen/settings/prod.py
+++ subgen/subgen/settings/prod.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET')
+secret_key_file = open('/home/django/secret_key.txt')
+SECRET_KEY = secret_key_file.readlines()[0]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
