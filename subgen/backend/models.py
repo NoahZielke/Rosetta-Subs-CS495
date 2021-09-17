1ef6537741a659f08caa0d1f05d577452bf31723
Logan Elkins <loganelkins@Logans-MacBook-Air.local>
2021-09-16 22:39:15 -0500

    Created api endpoint for the restful api

--- subgen/backend/models.py
+++ subgen/backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls.resolvers import URLPattern
 
-# Create your models here.
+class Job(models.Model):
+    name = models.CharField(max_length=100)
+    filename = models.TextField()
+    file = models.FileField(upload_to='uploads/', default='default/testAudio.mp3')
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100, default="Started")
+
+    class Meta:
+        app_label='backend'
+    def __str__(self):
+        return self.name
