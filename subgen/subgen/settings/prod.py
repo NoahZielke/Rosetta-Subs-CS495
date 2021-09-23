b00100f795b4d36acd5b6406fed43a1aa58cfae9
Logan Elkins <loganelkins@Logans-MacBook-Air.local>
2021-09-23 10:07:41 -0500

    Adjusted cronjob timings

--- subgen/subgen/settings/prod.py
+++ subgen/subgen/settings/prod.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_PASSWORD = open('/home/django/emailCreds.txt').readlines()[0]
 CRONJOBS = [
-    ('* * * * *', 'backend.cron.check_uploads', '--settings subgen.settings.prod'),
-    ('* * * * *', 'backend.cron.check_curr_jobs', '--settings subgen.settings.prod'),
-    ('* * * * *', 'backend.cron.check_all_jobs', '--settings subgen.settings.prod')
+    ('*/2 * * * *', 'backend.cron.check_uploads', '--settings subgen.settings.prod'),
+    ('*/1 * * * *', 'backend.cron.check_curr_jobs', '--settings subgen.settings.prod'),
+    ('*/5 * * * *', 'backend.cron.check_all_jobs', '--settings subgen.settings.prod')
 ]
