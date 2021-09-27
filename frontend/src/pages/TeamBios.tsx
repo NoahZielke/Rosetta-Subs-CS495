ab3a6460ac85ef8d1ac5953714e21148f6e7f120
Noah Zielke <noahzielke19@yahoo.com>
2021-09-27 17:52:17 -0500

    Add partial bios, app name (#6)
    * Add partial bios, app name
    
    * forgot my picture

--- frontend/src/pages/TeamBios.tsx
+++ frontend/src/pages/TeamBios.tsx
@@ -1,13 +1,14 @@
-import puffin from "../images/puffin.jpg";
 import teamLogo from "../images/app-logo-75px.png";
+import noah from "../images/noah-profile.jpg";
+import puffin from "../images/puffin.jpg";
 
 
 const TeamBios = () => {
-    function myFunction() {
-        var x = document.getElementById("contact-info")!;
+    function showHideContact(email:string, order:number) {
+        var x = document.getElementById(`contact-info-${order}`)!;
         if (x.style.display === "none") {
             x.style.display = "block";
-            x.innerHTML = "email: johnsmith@example.com";
+            x.innerHTML = `email: ${email}`;
         } else {
             x.style.display = "none";
         }
@@ -17,7 +18,7 @@ const TeamBios = () => {
             <div>
                 <nav className="navbar navbar-expand-md">
                     <a className="navbar-brand ps-3" href="/home" ><img src={teamLogo} alt="Translation app logo" /> </a>
-                    <a className="navbar-brand app-name" href="/home">Our App Name</a>
+                    <a className="navbar-brand app-name" href="/home">Rosetta-Subs</a>
                     <button className="navbar-toggler navbar-light" data-toggle="collapse" data-target="#collapse_target">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -40,18 +41,38 @@ const TeamBios = () => {
 
              <div className="row justify-content-center" style={{padding: "70px 0"}}>
                 <div className="col-10 row main-module ps-0">
-                    <img src={puffin} alt="A wonderful puffin bird" className="col-4 ps-0" style={{borderRadius: "10px"}} />
+                    <img src={puffin} alt="Picture of Logan Elkins" className="col-4 ps-0" style={{borderRadius: "10px"}} />
                     <div className="col-8">
-                        <h3 className="pt-4">Someone's Name</h3>
-                        <p className="pt-4 pb-2 px-2 main-module-text">
-                            I'm from such-and-such, my skills are so-and-so, my interests are so-and-so. I have worked here, and I've internshiped here, where I worked on this. 
+                        <h3 className="pt-4">Logan Elkins</h3>
+                        <p className="pt-4 px-2 biography-module-text">
+                            I am a senior at the University of Alabama graduating with a BS in Computer Science in December 2021. Throughout my internship at Synopsys, Inc., I have had the opportunity to specialize in Data Analytics via Python and Data Visualization in Tableau.
                         </p>
-                        <p className="pt-2 pb-3 px-2 main-module-text">
-                            More text here more text here more text here more text here more text here more text here more text here
+                        <p className="pt-2 px-2 biography-module-text">
+                            I have two dogs, an American Staffordshire and a Pointer/Lab mix, both of whom are rescues. I enjoy playing video games and going hiking in my free time.
                         </p>
-                        <div className="pt-2">
-                            <button onClick={myFunction} className="btn btn-primary py-2">Click to Show Contact Info</button>
-                            <p id="contact-info" className=" py-2 main-module-text" style={{display: "none"}}></p>
+                        <div className="pb-4 pt-2">
+                            <button onClick={() => showHideContact("lselkins@crimson.ua.edu", 1)} className="btn btn-primary py-2">Click to Show Contact Info</button>
+                            <p id="contact-info-1" className="pt-2 main-module-text" style={{display: "none"}}></p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div className="row justify-content-center" style={{padding: "15px 0"}}>
+                <div className="col-10 row main-module ps-0">
+                    <img src={noah} alt="Picture of Noah Zielke" className="col-4 ps-0" style={{borderRadius: "10px"}} />
+                    <div className="col-8">
+                        <h3 className="pt-4">Noah Samuel Zielke</h3>
+                        <p className="pt-4 px-2 biography-module-text">
+                            I have experience working with Ansible playbooks, basic C and C++, ROBOT Framework, Python, HTML/CSS, and very small amounts of Javascript. I also have some experience working with Openstack and Jenkins from my time as a Co-op at Adtran, where I worked on their device management software called the Mosaic Cloud Platform. 
+                        </p>
+                        <p className="pt-2 px-2 biography-module-text">
+                            I especially enjoy web design and automation work. Away from the computer, I enjoy hiking, biking, camping, studying Spanish, and playing basketball. Puedes me contactar para hablar en Espanol en cualquier momento!!! 
+                        </p>
+                        <div className="pb-4 pt-2">
+                            <button onClick={() => showHideContact("noahsamuelzielke@gmail.com", 2)} className="btn btn-primary py-2">Click to Show Contact Info</button>
+                            <p id="contact-info-2" className="pt-2 main-module-text" style={{display: "none"}}></p>
                         </div>
                         
                     </div>
