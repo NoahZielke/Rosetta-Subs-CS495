5ccf0a4f0b87144d07c14dbddba3b7f5041d31ba
Logan Elkins <loganelkins@Logans-MacBook-Air.local>
2021-10-05 09:53:13 -0500

    Small bug fix

--- subgen/backend/views.py
+++ subgen/backend/views.py
@@ -7,7 +7,9 @@ from .serializers import UserSerializer, GroupSerializer, JobSerializer
 from django.views.decorators.csrf import csrf_exempt
 from .models import Job
 from .utils import pullJSONGenSRTCompleted, sendFilesCompleted, pullJSONGenSRTFailed, sendFilesFailed, transcribeNewUploads
-import json
+import json, datetime
+from django.http import HttpResponse
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -27,18 +29,16 @@ class JobViewSet(viewsets.ModelViewSet):
     parser_classes = [parsers.MultiPartParser, parsers.FormParser]
     http_method_names = ['get', 'post', 'patch', 'delete']
     
-   def create(self, request, *args, **kwargs):
-       serializer = self.get_serializer(data=request.data)
-       serializer.is_valid(raise_exception=True)
-       self.perform_create(serializer)
-       headers = self.get_success_headers(serializer.data)
-       transcribeNewUploads()
-       return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        transcribeNewUploads()
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 @csrf_exempt
 def completed_job(request):
-    from django.http import HttpResponse
-    import datetime
     if request.method == 'POST':
         requestBodyStripped = (request.body).strip(b"'<>() ")
         requestBodyJson = requestBodyStripped.replace(b'\'', b'\"')
