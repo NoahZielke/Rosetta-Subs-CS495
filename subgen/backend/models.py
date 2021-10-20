825a6af8803ddef50b003f4bd7413b315d2bf9f4
Parker Hopson <pzhopson@crimson.ua.edu>
2021-10-20 17:13:13 -0500

    Copied backend back in locally, without accidental nesting

--- subgen/backend/models.py
+++ subgen/backend/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.urls.resolvers import URLPattern
-import uuid
+import uuid, random
 
 class Job(models.Model):
-    name = models.CharField(primary_key=True, max_length=32, default=str(uuid.uuid4()).replace('-', ''), editable=False)
     filename = models.TextField()
-    file = models.FileField(upload_to='uploads/', default='default/testAudio.mp3')
+    file = models.FileField(upload_to='media/uploads/', default='default/testAudio.mp3')
     date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100, default="Started")
     email_address = models.EmailField(max_length=254, default="test@domain.com")
