6b8a9b54c6c20e5b804cc6e120cfb3c41302a3eb
Noah Zielke <noahzielke19@yahoo.com>
2021-10-26 13:10:05 -0500

    Add video and sprint links, patch landing page and bio page (#33)
    * Add video and sprint links, patch landing page and bio page
    
    * Add Logan's profile pic to biopage
    
    * Add Parker's bio

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,40 +1,35 @@
-import { MyNavbar } from "../components/Navbar";
+import teamLogo from "../images/app-logo-75px.png";
+import Navbar from 'react-bootstrap/Navbar'
+import { Container, Nav} from "react-bootstrap";
 
 const LandingPage = () => {
   return (
     <div>
-      <div style={{ marginBottom: 50 }}>
-        <MyNavbar active='home' />
-      </div>
-
-      <div className='row' style={{ padding: "70px 0" }}>
-        <div className='col-8 main-module justify-content-center'>
-          <p className='pt-4 pb-2 px-2 main-module-text'>
-            Rosetta-Subs is an app designed to make adding subtitles to a video
-            in any language easy
-          </p>
-          <p className='main-module-text pb-3'>
-            This browser-based application requires no setup on the part of the
-            user
-          </p>
-          <div className='pb-4'>
-            <a href='/app-home' className='btn btn-success app-button'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='20'
-                height='20'
-                fill='currentColor'
-                className='bi bi-translate'
-                viewBox='0 0 16 16'>
-                <path d='M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z' />
-                <path d='M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z' />
-              </svg>
-              {/* icon source https://icons.getbootstrap.com/icons/translate/ */}
-              {"\u00A0"} Go to the Web App
+      <Navbar expand="lg" className="mr-0 ml-0">
+        <Container fluid>
+          <Navbar.Brand href="/home"><img src={teamLogo} alt='Translation app logo' />{" "}
+            {/* Image source: https://www.flaticon.com/free-icon/translation_2793580 */}
+          </Navbar.Brand>
+          <Navbar.Brand href="/home">
+            <a className='navbar-brand app-name' href='/home'>
+              Rosetta-Subs
             </a>
-         
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="navbar-item active px-3" href="/home">Home</Nav.Link>
+              <Nav.Link className="navbar-item px-3" href="/app-home">Web App</Nav.Link>
+              <Nav.Link className="navbar-item pl-3 pr-4" href="/team-bios">Team Member Bios</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="pb-5"></div>
+
       <div className="container-fluid">
-        <div className='row' style={{ padding: "70px 0" }}>
+        <div className='row pb-5 pt-4'>
           <div className='col-lg-8 col-11 main-module justify-content-center'>
             <p className='pt-4 pb-2 px-2 main-module-text'>
               Rosetta-Subs is an app designed to make adding subtitles to a video
@@ -42,7 +37,7 @@ const LandingPage = () => {
             </p>
             <p className='main-module-text pb-3'>
               This browser-based application requires no setup on the part of the
-              user
+              user:
             </p>
             <div className='pb-4'>
               <a href='/app-home' className='btn btn-success app-button'>
@@ -65,12 +60,11 @@ const LandingPage = () => {
       </div>
 
       <div className="container-fluid">
-        <div className='row'>
+        <div className='row pb-5 pt-3'>
           <div className='col-11 col-lg-8 main-module justify-content-center'>
             <p className='pt-4 pb-2 px-2 main-module-text'>
-              To view source code, instructions for running this app on your own
-              server, and videos presenting the app in use, visit our public
-              Github page
+              To view source code and instructions for running this app on your own
+              server, visit our public Github page:
             </p>
             <div className='pb-4'>
               <a
@@ -95,6 +89,84 @@ const LandingPage = () => {
         </div>
       </div>
 
+      <div className="container-fluid">
+        <div className='row pt-3 pb-5'>
+          <div className='col-11 col-lg-8 main-module justify-content-center'>
+            <p className='pt-4 pb-2 px-2 main-module-text'>
+              View a video demonstrating the app's functionality:
+            </p>
+            <div className="pb-4">
+              <a
+                href='https://www.youtube.com/watch?v=zkiebh9p5mE&ab_channel=NoahZielke'
+                target='_blank'
+                rel='noreferrer'
+                className='btn btn-dark'>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  className="bi bi-youtube" 
+                  viewBox="0 0 16 16">
+                  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                </svg>
+                {/* Icon source https://icons.getbootstrap.com/icons/youtube/ */}
+                {"\u00A0"} Video Tour of App 
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className='row pt-3 pb-5'>
+          <div className='col-11 col-lg-8 main-module justify-content-center'>
+            <p className='pt-4 pb-2 px-2 main-module-text'>
+              View our individual sprint progress (UA box credentials required):
+            </p>
+            <div className='pb-4'>
+              <a
+                href='https://alabama.app.box.com/folder/146606344889'
+                target='_blank'
+                rel='noreferrer'
+                className='btn btn-danger'>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  className="bi bi-arrow-up-right-square" 
+                  viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                </svg>
+                {/* Icon source https://icons.getbootstrap.com/icons/arrow-up-right-square/ */}
+                {"\u00A0"} Sprint 1 Deliverables
+              </a>
+            </div>
+            <div className="pb-4">
+              <a
+                href='https://alabama.app.box.com/folder/146234329999'
+                target='_blank'
+                rel='noreferrer'
+                className='btn btn-info'>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  className="bi bi-arrow-up-right-square" 
+                  viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                </svg>
+                {/* Icon source https://icons.getbootstrap.com/icons/arrow-up-right-square/ */}
+                {"\u00A0"} Sprint 2 Deliverables
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
       <footer></footer>
 
       <div className='py-2 px-3 attribution'>
@@ -107,9 +179,6 @@ const LandingPage = () => {
           www.flaticon.com
         </a>
       </div>
-    </div>
-    </div>
-    </div>
     </div>
   );
 };
--- frontend/src/pages/TeamBios.tsx
+++ frontend/src/pages/TeamBios.tsx
@@ -1,4 +1,7 @@
-import { MyNavbar } from "../components/Navbar";
+import Navbar from 'react-bootstrap/Navbar'
+import { Container, Nav } from "react-bootstrap";
+import teamLogo from "../images/app-logo-75px.png";
+import logan from "../images/logan-profile.jpg";
 import noah from "../images/noah-profile.jpg";
 import puffin from "../images/puffin.jpg";
 
@@ -14,12 +17,100 @@ const TeamBios = () => {
   }
   return (
     <div>
-      <div style={{ marginBottom: 50 }}>
-        <MyNavbar active='team member bios' />
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="/home"><img src={teamLogo} alt='Translation app logo' />{" "}
+            {/* Image source: https://www.flaticon.com/free-icon/translation_2793580 */}
+          </Navbar.Brand>
+          <Navbar.Brand href="/home">
+            <a className='navbar-brand app-name' href='/home'>
+              Rosetta-Subs
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="navbar-item px-3" href="/home">Home</Nav.Link>
+              <Nav.Link className="navbar-item px-3" href="/app-home">Web App</Nav.Link>
+              <Nav.Link className="navbar-item active pl-3 pr-4" href="/team-bios">Team Member Bios</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="container-fluid">
+        <div className='row justify-content-center' style={{ padding: "70px 0" }}>
+          <div className='col-10 row main-module pl-0'>
+            <img
+              src={logan}
+              alt='Logan Elkins'
+              className='d-none d-lg-block col-lg-4 pl-0'
+              style={{ borderRadius: "10px" }}
+            />
+            <div className='col-lg-8 col-12'>
+              <h3 className='pt-4'>Logan Elkins</h3>
+              <p className='pt-3 px-2 biography-module-text'>
+                I am a senior at the University of Alabama graduating with a BS in
+                Computer Science in December 2021. Throughout my internship at
+                Synopsys, Inc., I have had the opportunity to specialize in Data
+                Analytics via Python and Data Visualization in Tableau.
+              </p>
+              <p className='pt-1 px-2 biography-module-text'>
+                I have two dogs, an American Staffordshire and a Pointer/Lab mix,
+                both of whom are rescues. I enjoy playing video games and going
+                hiking in my free time.
+              </p>
+              <div className='pb-4 pt-2'>
+                <button
+                  onClick={() => showHideContact("lselkins@crimson.ua.edu", 1)}
+                  className='btn btn-primary py-2'>
+                  Click to Show Contact Info
+                </button>
+                <p
+                  id='contact-info-1'
+                  className='pt-2 main-module-text'
+                  style={{ display: "none" }}></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="container-fluid">
         <div className='row justify-content-center' style={{ padding: "15px 0" }}>
+          <div className='col-10 row main-module pl-0'>
+            <img
+              src={puffin}
+              alt='Parker Hopson'
+              className='d-none d-lg-block col-lg-4 pl-0'
+              style={{ borderRadius: "10px" }}
+            />
+            <div className='col-lg-8 col-12'>
+              <h3 className='pt-4'>Parker Hopson</h3>
+              <p className='pt-3 px-2 biography-module-text'>
+                I am a senior at the University of Alabama, expecting to graduate in December with a B.S. in Computer Science, Minor in Mathematics. I have experience in multiple facets of IT, from triage to basic server maintenance, from my time at Alabama Public Radio and WVUA23 News station.
+              </p>
+              <p className='px-2 biography-module-text'>
+                I also gained experience in project management during my time at Profill Solutions. Other than technology, my interests include exercising, watching movies, and listening to a variety of music.
+              </p>
+              <div className='pb-4 pt-2'>
+                <button
+                  onClick={() => showHideContact("pzhopson@crimson.ua.edu", 2)}
+                  className='btn btn-primary py-2'>
+                  Click to Show Contact Info
+                </button>
+                <p
+                  id='contact-info-2'
+                  className='pt-2 main-module-text'
+                  style={{ display: "none" }}></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className='row justify-content-center' style={{ padding: "70px 0" }}>
           <div className='col-10 row main-module pl-0'>
             <img
               src={noah}
@@ -44,12 +135,12 @@ const TeamBios = () => {
               </p>
               <div className='pb-4 pt-2'>
                 <button
-                  onClick={() => showHideContact("noahsamuelzielke@gmail.com", 2)}
+                  onClick={() => showHideContact("noahsamuelzielke@gmail.com", 3)}
                   className='btn btn-primary py-2'>
                   Click to Show Contact Info
                 </button>
                 <p
-                  id='contact-info-2'
+                  id='contact-info-3'
                   className='pt-2 main-module-text'
                   style={{ display: "none" }}></p>
               </div>
@@ -59,7 +150,7 @@ const TeamBios = () => {
       </div>
 
       <div className="container-fluid">
-        <div className='row justify-content-center' style={{ padding: "70px 0" }}>
+        <div className='row justify-content-center' style={{ padding: "15px 0" }}>
           <div className='col-10 row main-module pl-0'>
             <img
               src={puffin}
@@ -76,12 +167,12 @@ const TeamBios = () => {
               </p>
               <div className='pb-4 pt-2'>
                 <button
-                  onClick={() => showHideContact("crmilsap@crimson.ua.edu", 3)}
+                  onClick={() => showHideContact("crmilsap@crimson.ua.edu", 4)}
                   className='btn btn-primary py-2'>
                   Click to Show Contact Info
                 </button>
                 <p
-                  id='contact-info-3'
+                  id='contact-info-4'
                   className='pt-2 main-module-text'
                   style={{ display: "none" }}></p>
               </div>
