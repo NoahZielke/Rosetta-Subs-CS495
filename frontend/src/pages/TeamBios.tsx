147fbd73e1bbd8a55553f6d70ad1631bc0aa16e0
Noah Zielke <noahzielke19@yahoo.com>
2021-10-26 09:41:41 -0500

    Add video and sprint links, patch landing page and bio page

--- frontend/src/pages/TeamBios.tsx
+++ frontend/src/pages/TeamBios.tsx
@@ -1,4 +1,6 @@
-import { MyNavbar } from "../components/Navbar";
+import Navbar from 'react-bootstrap/Navbar'
+import { Container, Nav } from "react-bootstrap";
+import teamLogo from "../images/app-logo-75px.png";
 import noah from "../images/noah-profile.jpg";
 import puffin from "../images/puffin.jpg";
 
@@ -14,8 +16,63 @@ const TeamBios = () => {
   }
   return (
     <div>
-      <div style={{ marginBottom: 50 }}>
-        <MyNavbar active='team member bios' />
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="/home"><img src={teamLogo} alt='Translation app logo' />{" "}
+            {/* Image source: https://www.flaticon.com/free-icon/translation_2793580 */}
+          </Navbar.Brand>
+          <Navbar.Brand href="/home">
+            <a className='navbar-brand app-name' href='/home'>
+              Rosetta-Subs
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="navbar-item px-3" href="/home">Home</Nav.Link>
+              <Nav.Link className="navbar-item px-3" href="/app-home">Web App</Nav.Link>
+              <Nav.Link className="navbar-item active pl-3 pr-4" href="/team-bios">Team Member Bios</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="container-fluid">
+        <div className='row justify-content-center' style={{ padding: "70px 0" }}>
+          <div className='col-10 row main-module pl-0'>
+            <img
+              src={puffin}
+              alt='Logan Elkins'
+              className='d-none d-lg-block col-lg-4 pl-0'
+              style={{ borderRadius: "10px" }}
+            />
+            <div className='col-lg-8 col-12'>
+              <h3 className='pt-4'>Logan Elkins</h3>
+              <p className='pt-3 px-2 biography-module-text'>
+                I am a senior at the University of Alabama graduating with a BS in
+                Computer Science in December 2021. Throughout my internship at
+                Synopsys, Inc., I have had the opportunity to specialize in Data
+                Analytics via Python and Data Visualization in Tableau.
+              </p>
+              <p className='pt-1 px-2 biography-module-text'>
+                I have two dogs, an American Staffordshire and a Pointer/Lab mix,
+                both of whom are rescues. I enjoy playing video games and going
+                hiking in my free time.
+              </p>
+              <div className='pb-4 pt-2'>
+                <button
+                  onClick={() => showHideContact("lselkins@crimson.ua.edu", 1)}
+                  className='btn btn-primary py-2'>
+                  Click to Show Contact Info
+                </button>
+                <p
+                  id='contact-info-1'
+                  className='pt-2 main-module-text'
+                  style={{ display: "none" }}></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="container-fluid">
