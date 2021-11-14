f82d5cd98270ea89932f7d5cd7c49b18b8e4ea75
Logan Elkins <loganelkins@Logans-MacBook-Air.local>
2021-11-14 10:57:04 -0600

    Required changes to views and utils

--- subgen/backend/views.py
+++ subgen/backend/views.py
@@ -40,7 +40,8 @@ class JobViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        transcribeNewUploads()
+        mostRecentJob = Job.objects.latest('id')
+        transcribeNewUploads(mostRecentJob)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 @csrf_exempt
